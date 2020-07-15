65042829bfdff7bb9c23d8a284501a0cfa7e6e6a
Julien Bonjean <julien@bonjean.info>
2020-07-14 20:21:20 -0400

    fix build

--- build-images.sh
+++ build-images.sh
@@ -20,6 +20,7 @@ DOCKER_TAG="${DOCKER_TAG:-master}"
 for image in $IMAGES; do
 	echo "Building jbonjean/$image"
 	docker build "$image" --tag "jbonjean/$image:$DOCKER_TAG" --build-arg DOCKER_TAG="$DOCKER_TAG"
+	docker tag "jbonjean/$image:$DOCKER_TAG" "jbonjean/$image:latest"
 done
 
 echo "Build success"
@@ -38,7 +39,6 @@ done
 # Update latest tag.
 for image in $IMAGES; do
 	echo "Pushing jbonjean/$image:latest"
-	docker tag "jbonjean/$image:$DOCKER_TAG" "jbonjean/$image:latest"
 	docker push "jbonjean/$image:latest"
 done
 
